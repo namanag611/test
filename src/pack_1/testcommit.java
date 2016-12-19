919a31eb74c706b89f322ca1feaffeacca9ced36
NAMAN <NAMAN@NAMAN-PC>
2016-12-19 20:44:13 +0530

    test naman3

--- src/pack_1/testcommit.java
+++ src/pack_1/testcommit.java
@@ -1,6 +1,37 @@
 package pack_1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class testcommit {
 
-	//this is test ghjg  hgfhdfh g
+	@Test
+	public void SQLConnection() {
+		// this is test ghjg hgfhdfh g
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+			// Creating a connection to the database
+			Connection conn = DriverManager.getConnection("jdbc:sqlserver://artOfTesting.com/testDB", "userName",
+					"password");
+
+			// Executing SQL query and fetching the result
+			Statement st = conn.createStatement();
+			String sqlStr = "select * from testTable";
+			ResultSet rs = st.executeQuery(sqlStr);
+			while (rs.next()) {
+				System.out.println(rs.getString("name"));
+			}
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+			Assert.fail();
+			// this is test
+		}
+	}
 }
